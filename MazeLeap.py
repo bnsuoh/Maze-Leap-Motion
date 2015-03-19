c05a6e95930f9804b823d7fa1fc9031c8d5b437b
bnsuoh <bnsue1@gmail.com>
2015-03-19 13:27:25 +0200

    shortened

--- MazeLeap.py
+++ MazeLeap.py
@@ -7,26 +7,21 @@ class SampleListener(Leap.Listener):
     def on_connect(self, controller):
         print "Connected"
     def on_frame(self, controller):
-        width = 180
         frame = controller.frame()
         hand = frame.hands.rightmost
         handCenter = hand.palm_position
-        posX=int((handCenter.x/200)*(width/2)+width/2)
-        posY=(handCenter.z/200)*width
+        posX=int((handCenter.x/200)*(90)+90)
+        posY=int((handCenter.y/200)*(90)+90)
         print ("x: " + str(posX) + " y: " + str(posY))
         ser.write(str(posX))
         time.sleep(0.07)
-
 def main():
     listener = SampleListener()
     controller = Leap.Controller()
     controller.add_listener(listener)
-    # Keep this process running until Enter is pressed
-    print "Press Enter to quit..."
     try:
         sys.stdin.readline()
     except KeyboardInterrupt:
         pass
-
 if __name__ == "__main__":
     main()
