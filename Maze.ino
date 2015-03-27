20bcf87cdc8bbd0a966b04203356365b2945e6a2
bnsuoh <bnsue1@gmail.com>
2015-03-27 18:18:20 +0200

    fixes

--- Maze.ino
+++ Maze.ino
@@ -9,29 +9,31 @@
 
 #include <Servo.h> 
 Servo serX;  // create serX object to control a serX 
-int pos = 0;    // variable to store the serX position 
-String inputString = "";
  
 void setup() { 
   Serial.begin(9600);
   serX.attach(9);  // attaches the serX on pin 9 to the serX object 
   serX.write(0);
+  delay(500);
 } 
  
 void loop() {
-  while(Serial.available()>0){
-    int x=Serial.parseInt();
+  while(Serial.available() && Serial.read()>0){
+    int x=int(Serial.read());
     turn(x);
+    delay(5);
   }
 } 
 void turn(int x){
+  int a=90;
   if (x<=0){
-    serX.write(0);
+    a=0;
   }
-  else if(x>=180){
-    serX.write(180);
+  else if(x>180){
+    a=180;
   }
   else{
-    serX.write(x);
+    a=x;
   }
+  serX.write(a);
 }
