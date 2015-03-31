c877558dfba1b3deac9fc3b994b84029907a317a
bnsuoh <bnsue1@gmail.com>
2015-03-31 10:42:56 +0300

    added comments

--- MazeLeap.py
+++ MazeLeap.py
@@ -1,21 +1,19 @@
+#by Bensu Sicim
 import sys, thread, time, Leap, serial
 #sys.path.insert(0, "Desktop/Maze/lib")
 from serial import Serial
-from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 ser = serial.Serial('/dev/tty.usbmodem1421', 9600)
-posXprev=90
 class SampleListener(Leap.Listener):
     def on_connect(self, controller):
         print "Connected"
     def on_frame(self, controller):
-        global posXprev
         frame = controller.frame()
-        hand = frame.hands.rightmost
-        handCenter = hand.palm_position
-        posX=(int((handCenter.x/200)*(90)+90))+100
-        posY=(int((handCenter.y/200)*(90)+90))+100
+        hand = frame.hands.rightmost #create rightmost hand
+        handCenter = hand.palm_position #get hand palm position
+        posX=(int((handCenter.x/200)*(90)+90))+100 #get x coordinate position and convert it to [100,280]
+        posY=(int((handCenter.y/200)*(90)+90))+100 #+100 is to equalize the number of digits
         print ("x: " + str(posX) + " y: " + str(posY))
-        ser.write(str(posX)+str(posY))
+        ser.write(str(posX)+str(posY)) #writes the positions as a 6 digit value
         time.sleep(0.1)
 def main():
     listener = SampleListener()
