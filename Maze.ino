c92ac995397fd33be937df6c9144fa766e1f69cd
bnsuoh <bnsue1@gmail.com>
2015-04-13 21:07:52 +0300

    maze final

--- Maze.ino
+++ Maze.ino
@@ -6,23 +6,62 @@
 
 #include <Servo.h> 
 Servo serX,serY;  // create servo objects
-int turnX,turnY,pos,x,y;
+int turnX,turnY,pos,xAng,yAng;
+int x=90;
+int y=90;
+String a,b;
 int beforeX=0; //set initial positions to 0
 int beforeY=0;
 void setup() { 
   Serial.begin(9600);
   serX.attach(9);  // attach the serX on pin 9
   serY.attach(8); // attach the serY on pin 10
+  serX.write(90);
+  serY.write(90);
+  xAng=90;
+  yAng=90;
+
 } 
 void loop() {
   char buffer[] = {' ',' ',' ',' ',' ',' '}; // receive up to 6 bytes
   if(Serial.available()){
      Serial.readBytesUntil('n', buffer, 6); //read data
-     x = (((int)buffer[0]-48)*100+((int)buffer[1]-48)*10+((int)buffer[2]-48))-100; //get the first 3 digits of buffer and convert them into int
-     y = (((int)buffer[3]-48)*100+((int)buffer[4]-48)*10+((int)buffer[2]-48))-100; //get the second 3 digits " " " " " " " " 
-     turn();
+     a=String(buffer[0])+String(buffer[1])+String(buffer[2]);
+     b=String(buffer[3])+String(buffer[4])+String(buffer[5]);
+     x=a.toInt()-100;
+     y=b.toInt()-100;
+     /*x = (((int)buffer[0])*100+((int)buffer[1])*10+((int)buffer[2]))-100; //get the first 3 digits of buffer and convert them into int
+     y = (((int)buffer[3])*100+((int)buffer[4])*10+((int)buffer[2]))-100; //get the second 3 digits " " " " " " " " */
   }
-} 
+     if(x>90){
+       xAng+=5;
+     }
+     else if(x<90){
+       xAng-=5;
+     }
+     
+     if(y>90){
+       yAng+=5;
+     }
+     else if(y<90){
+       yAng-=5;
+     }
+     if(xAng<=0){
+       xAng=0;
+     }
+     else if(xAng>=180){
+       xAng=180;
+     }
+     if(yAng<=0){
+       yAng=0;
+     }
+     else if(yAng>=180){
+       yAng=180;
+     }
+     serX.write(xAng);
+     serY.write(yAng);
+     delay(50);
+}
 void turn(){
   if (x<=0){ //give value to turn between [0,180]
     turnX=0;
@@ -42,6 +81,10 @@ void turn(){
   else{
     turnY=y;
   }
+  /*serX.write(turnX);
+  delay(100);
+  serY.write(turnY);
+  delay(100);*/
   if(turnX>beforeX && turnY>beforeY){ //increase the angle if turn is greater than the previous angle, do the opposite if not
     for(pos=beforeX;pos<=turnX;pos+=1){
       serX.write(pos);
@@ -83,5 +126,5 @@ void turn(){
     }
   }
   beforeY=turnY; //change previous angle
-  beforeX=turnX; //change previous angle
+  beforeX=turnX; //change previous angle*/
 }
--- MazeLeap.py
+++ MazeLeap.py
@@ -3,18 +3,34 @@ import sys, thread, time, Leap, serial
 #sys.path.insert(0, "Desktop/Maze/lib")
 from serial import Serial
 ser = serial.Serial('/dev/tty.usbmodem1421', 9600)
+beforeX,beforeY=190,190
 class SampleListener(Leap.Listener):
     def on_connect(self, controller):
         print "Connected"
     def on_frame(self, controller):
+        global beforeX,beforeY
         frame = controller.frame()
         hand = frame.hands.rightmost #create rightmost hand
         handCenter = hand.palm_position #get hand palm position
         posX=(int((handCenter.x/200)*(90)+90))+100 #get x coordinate position and convert it to [100,280]
         posY=(int((handCenter.z/200)*(90)+90))+100 #+100 is to equalize the number of digits
+        if(posX<=100):
+            posX=100
+        elif(posX>=280):
+            posX=280
+        elif(posX==190):
+            posX=beforeX
+        if(posY<=100):
+            posY=100
+        elif(posY>=280):
+            posY=280
+        elif(posY==190):
+            posY=beforeY
+        beforeX=posX
+        beforeY=posY
         print ("x: " + str(posX) + " y: " + str(posY))
         ser.write(str(posX)+str(posY)) #writes the positions as a 6 digit value
-        time.sleep(0.1)
+        time.sleep(0.05)
 def main():
     listener = SampleListener()
     controller = Leap.Controller()
